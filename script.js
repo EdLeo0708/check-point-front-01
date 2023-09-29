6f9ed9420a273e822a72334d802300be4b769ef0
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 11:01:16 -0300

    check-point-front-01

---
+++ script.js
@@ -0,0 +1,30 @@
+// VARIAVEIS 
+let nome = "Edson Leonardo";
+let idade = 19;
+let status = true;
+let Valorproduto = 10.50;
+let listaTimes = ["Corinthians, Boca junior, Santos"];
+
+//PRINT
+console.log(nome);
+console.log(idade);
+console.log(status);
+console.log(Valorproduto);
+console.log(listaTimes);
+
+//NUMEROS
+let n1 = 10
+let n2 = 2
+
+//SOMA - DIVISÃO - SUBTRAÇÂO - MULTIPLIAÇÂO
+
+let SOMA = n1 + n2;
+let DIVISÃO = n1 / n2;
+let SUBTRAÇÂO = n1 - n2;
+let MULTIPLIAÇÂO = n1 * n2;
+
+//print
+console.log("O resultado da soma é ", {SOMA})
+console.log("O resultado da divisão é ", {DIVISÃO})
+console.log("O resultado da subtração é ", {SUBTRAÇÂO})
+console.log("O resultado da multiplicação é ", {MULTIPLIAÇÂO})
